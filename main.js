90114ad03ee339522609db0836295f267d5de891
Abhash <abhash24oct@gamil.com>
2018-08-27 23:01:27 +0530

    Second commit

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ document.addEventListener("DOMContentLoaded",function(){
     console.log("Content loaded");
     var numberdivs=document.getElementsByClassName("number");
 
-
+    var __ = document.getElementsByClassName("button");
+    for (var i=0; i<__.length; i++){
+        __[i].addEventListener("click", function(e){
+            updateScreen(e.target.getAttribute("id"));
+        });
+    }
 
 });
 
@@ -13,6 +18,10 @@ document.addEventListener("DOMContentLoaded",function(){
  var num2;
  var lastOp="";
  var equalsFlag=false;
+
+
+
+
 
 
 function updateScreen(id){
@@ -28,6 +37,8 @@ function updateScreen(id){
       num2=null;
       upperScreen.innerText="";
       lowerScreen.innerText="";
+      lastOp="";
+      equalsFlag=false;
     }else if (id=="CE") {
       lowerText=0;
       lowerScreen.innerText=lowerText;
@@ -51,7 +62,28 @@ function updateScreen(id){
             upperText="";
             num1=lowerText;
             num2=undefined;
-          }
+        }else if(lastOp=="-"){
+            lowerText=num1-num2;
+            lowerScreen.innerText=lowerText;
+            upperScreen.innerText="";
+            upperText="";
+            num1=lowerText;
+            num2=undefined;
+        }else if(lastOp=="*"){
+            lowerText=num1*num2;
+            lowerScreen.innerText=lowerText;
+            upperScreen.innerText="";
+            upperText="";
+            num1=lowerText;
+            num2=undefined;
+        }else if(lastOp=="/"){
+            lowerText=num1/num2;
+            lowerScreen.innerText=lowerText;
+            upperScreen.innerText="";
+            upperText="";
+            num1=lowerText;
+            num2=undefined;
+        }
       }else{
         lowerScreen.innerText=lowerText;
         upperScreen.innerText="";
@@ -79,19 +111,20 @@ function updateScreen(id){
 
       }else{
           console.log("Operation clicked  upperText:: " +upperText+" lowerText::"+lowerText+" num1 ::"+num1+" num2:: "+num2);
-          lastOp=id;
           if(equalsFlag && num1!=null){
             lowerText=num1;
           }else{
+
             if(lowerText!=""){
+
               if(num1==null){
                 num1=parseInt(lowerText);
               }else{
                 num2=parseInt(lowerText);
               }
             }
-        }
-
+          }
+            //Add operation
           if(id=="+"){
 
             if(upperText!=""){
@@ -104,6 +137,48 @@ function updateScreen(id){
             }
 
           }
+
+           //Subtract 
+           if(id=="-"){
+
+                if(upperText!=""){
+                upperText=upperText+lowerText+"-" ;
+                upperScreen.innerText=upperText;
+                }else{
+                upperText=lowerText+"-" ;
+                upperScreen.innerText=upperText;
+
+                }
+
+          }
+
+          //Multiply
+            if(id=="*"){
+
+                    if(upperText!=""){
+                    upperText=upperText+lowerText+"X" ;
+                    upperScreen.innerText=upperText;
+                    }else{
+                    upperText=lowerText+"X" ;
+                    upperScreen.innerText=upperText;
+
+                    }
+
+            }
+
+          //Divide
+            if(id=="/"){
+
+                    if(upperText!=""){
+                    upperText=upperText+lowerText+"/" ;
+                    upperScreen.innerText=upperText;
+                    }else{
+                    upperText=lowerText+"/" ;
+                    upperScreen.innerText=upperText;
+
+                    }
+
+            }
           if(upperText!="" && num1!=undefined && num2!=undefined && !equalsFlag) {
 
             if(lastOp=="+"){
@@ -112,9 +187,31 @@ function updateScreen(id){
               num1=lowerText;
               num2=undefined;
             }
+
+            if(lastOp=="-"){
+              lowerText=num1-num2;
+              lowerScreen.innerText=lowerText;
+              num1=lowerText;
+              num2=undefined;
+            }
+
+            if(lastOp=="*"){
+              lowerText=num1*num2;
+              lowerScreen.innerText=lowerText;
+              num1=lowerText;
+              num2=undefined;
+            }
+
+            if(lastOp=="/"){
+              lowerText=num1/num2;
+              lowerScreen.innerText=lowerText;
+              num1=lowerText;
+              num2=undefined;
+            }
           }
           lowerText="";
           console.log(" aFTER OPERATION num1::"+num1+" num2::"+num2);
+          lastOp=id;
       }
 
   }
